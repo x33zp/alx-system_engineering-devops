8051f7b013ed47103847d086b4630bd2ebf1fa3d
zubby peculiar <zubbypeculiar@gmail.com>
2024-03-13 17:10:07 +0100

    fix

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # A regular expression, matches a string that starts with h ends with n and has a single character in between
-puts ARGV[0].scan(/^{0-9}{10}$/).join
+puts ARGV[0].scan(/^[0-9]{10}$/).join
