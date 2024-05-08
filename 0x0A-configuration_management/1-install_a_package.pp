51aa09eadc912570f010d02a5eb2363d4192fabc
x33zp <zubbypeculiar@gmail.com>
2024-05-08 04:51:21 -0700

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,5 @@
+# Puppet manifest to install flask using pip3
+
 package { 'flask':
   ensure => '2.1.0',
   provider => 'pip3',
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,5 @@
+# Puppet manifest to kill a process named killmenow using `pkill`
+
 exec { 'killmenow':
   command => 'pkill -f killmenow',
   path    => ['/usr/bin', '/bin'],
