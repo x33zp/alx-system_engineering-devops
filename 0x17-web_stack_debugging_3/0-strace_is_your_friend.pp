d04c740bd9b6c9aede1a73ef6c2ffa9bb4c20408
x33zp <zubbypeculiar@gmail.com>
2024-08-19 00:24:42 +0100

    puppet to fix apache

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# A puppet manifest that fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
