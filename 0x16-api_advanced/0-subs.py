ceac442de31fc9136eca15521570899864364bbe
x33zp <zubbypeculiar@gmail.com>
2024-08-20 14:44:07 +0100

    last req fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Qeries the Reddit API and returns the number of subscribers """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
 
     url = 'https://api.reddit.com/r/{}/about'.format(subreddit)
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0"}
