687ffdd6f36512c7bb510dc8404cf8ae99452d54
x33zp <zubbypeculiar@gmail.com>
2024-08-20 14:12:19 +0100

    subs count api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" A function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given subreddit.
+    If an invalid subreddit is given, the function should return 0. """
+import requests
+
+def number_of_subscribers(subreddit):
+    url = 'https://api.reddit.com/r/{}/about'.format(subreddit)
+
+    res = requests.get(url, allow_redirects=False)
+    result  = res.json()
+
+    try:
+        return result.get('data').get('subscribers')
+    except Exception as e:
+        return 0
