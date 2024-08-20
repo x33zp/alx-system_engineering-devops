8839fec9410b0f0d0f89da770361403eb0932be8
x33zp <zubbypeculiar@gmail.com>
2024-08-20 18:22:00 +0100

    code fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,23 +6,23 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=None):
     """Recursively query Reddict API and return a list"""
-    url = 'https://api.reddit.com/r/{}/hot'.format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0"}
-    params = {"limit": 100, "after": after}
-
-    res = requests.get(url, headers=headers, params=params,
-                       allow_redirects=False)
-
     try:
-        result = res.json()
+        url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+        headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0"}
+        params = {"limit": 100, "after": after}
+
+        req = requests.get(url, headers=headers,
+                               params=params, allow_redirects=False)
+
+        result = req.json().get('data')
+
+        after = result.get('after')
+
+        for child in result.get('children'):
+            hot_list.append(child.get('data').get('title'))
+        if after is not None:
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
     except Exception:
-        return None
-
-    after = result.get('data').get('after')
-
-    for child in result.get('data').get('children'):
-        hot_list.append(child.get('data').get('title'))
-    if after is not None:
-        recurse(subreddit, hot_list, after)
-    else:
-        return hot_list
+        print(None)
