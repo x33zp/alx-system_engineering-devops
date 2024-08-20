44c0c8a289d4d3821693e128b4cefe301a7363e5
x33zp <zubbypeculiar@gmail.com>
2024-08-20 14:26:29 +0100

    req fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" A function that queries the Reddit API and returns the number of
-    subscribers (not active users, total subscribers) for a given subreddit.
-    If an invalid subreddit is given, the function should return 0. """
+'''
+A function that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
+'''
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """ Queries the Reddit API and returns the number of subscribers """
     url = 'https://api.reddit.com/r/{}/about'.format(subreddit)
-    headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0"
-            }
+    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0'}
 
     res = requests.get(url, headers=headers, allow_redirects=False)
     result = res.json()
