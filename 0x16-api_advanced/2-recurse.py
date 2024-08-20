015f3aa343442bfb0de3707a9404d1bdbf2ad367
x33zp <zubbypeculiar@gmail.com>
2024-08-20 17:37:55 +0100

    request recursively, with pagination

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Reddit API"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """Recursively query Reddict API and return a list"""
+    url = 'https://api.reddit.com/r/{}/hot'.format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0"}
+    params = {"limit": 100, "after": after}
+
+    res = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    try:
+        result = res.json()
+    except Exception:
+        return None
+
+    after = result.get('data').get('after')
+
+    for child in result.get('data').get('children'):
+        hot_list.append(child.get('data').get('title'))
+    if after is not None:
+        recurse(subreddit, hot_list, after)
+    else:
+        return hot_list
