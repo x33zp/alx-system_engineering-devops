7aaf19edad5202d0eb6c1830e048db62c176fc8a
x33zp <zubbypeculiar@gmail.com>
2024-08-20 14:17:09 +0100

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,11 @@ import requests
 def number_of_subscribers(subreddit):
     """ Queries the Reddit API and returns the number of subscribers """
     url = 'https://api.reddit.com/r/{}/about'.format(subreddit)
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0"
+            }
 
-    res = requests.get(url, allow_redirects=False)
+    res = requests.get(url, headers=headers, allow_redirects=False)
     result = res.json()
 
     try:
