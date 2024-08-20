ba41310bd81bc6b8194a8e8fa2ae3a574828ffc5
x33zp <zubbypeculiar@gmail.com>
2024-08-20 14:31:34 +0100

    req fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-'''
-A function that queries the Reddit API and returns the number of
-subscribers (not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0.
-'''
+""" A function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given subreddit.
+    If an invalid subreddit is given, the function should return 0. """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Queries the Reddit API and returns the number of subscribers """
-    url = 'https://api.reddit.com/r/{}/about'.format(subreddit)
-    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0'}
-
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    result = res.json()
-
+    """ A function that queries the Reddit APIand returns
+        the number of subscribers """
     try:
-        return result.get('data').get('subscribers')
+        url = "https://api.reddit.com/r/{}/about".format(subreddit)
+        headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0"
+            }
+        request = requests.get(url, headers=headers, allow_redirects=False)
+        '''if request.status_code == 404:
+            return (0)'''
+        subscribers = request.json().get('data').get('subscribers')
+        return subscribers
     except Exception:
-        return 0
+        return (0)
